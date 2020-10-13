5394b9907d84f36a2bdf0b3665f2cd9369a13b33
Hank Liu <hanke.liu@gmail.com>
2020-10-13 15:05:20 -0400

    test workflow and annotations

--- index.ts
+++ index.ts
@@ -2,6 +2,26 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const logTodo = (id: number, title: string, finished: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has title of: ${title}
+    Is it finished? ${finished}
+    `)
+}
+
 axios.get(url).then(res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  logTodo(id, title, finished);
 })
